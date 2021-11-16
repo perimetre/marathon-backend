5d2a68f2549748a9900cca1d6c4ff057695c2c56
Alessandro Macanha <alessandromacanha@gmail.com>
2021-11-16 08:45:31 -0300

    fix: added makeError

--- src/resolvers/user/index.ts
+++ src/resolvers/user/index.ts
@@ -1,6 +1,7 @@
 import { mutationField, nonNull, nullable, arg, inputObjectType } from 'nexus';
 import axios, { AxiosResponse } from 'axios';
 import { env } from '../../env';
+import { makeError } from '../../utils/exception';
 
 export const UserSingIn = inputObjectType({
   name: 'UserSingIn',
@@ -15,34 +16,41 @@ export const UserMutations = [
     type: nullable('Session'),
     args: { user: nonNull(arg({ type: 'UserSingIn' })) },
     resolve: async (_parent, args, ctx) => {
-      const { user } = args;
-      const request = (await axios({
-        method: 'POST',
-        url: env.MARATHON_API_LOGIN,
-        headers: {
-          Authorization: `Basic ${Buffer.from(`${user.email}:${user.password}`).toString('base64')}`
-        }
-      })) as AxiosResponse<{ user_id: number; user_token: string }>;
+      try {
+        const { user } = args;
+        const request = (await axios({
+          method: 'POST',
+          url: env.MARATHON_API_LOGIN,
+          headers: {
+            Authorization: `Basic ${Buffer.from(`${user.email}:${user.password}`).toString('base64')}`
+          }
+        })) as AxiosResponse<{ user_id: number; user_token: string }>;
 
-      if (request.status === 200) {
-        let verifyUser = await ctx.prisma.user.findFirst({ where: { email: user.email as string } });
-        if (!verifyUser) {
-          verifyUser = await ctx.prisma.user.create({
+        if (request.status === 200) {
+          let verifyUser = await ctx.prisma.user.findFirst({ where: { email: user.email as string } });
+          if (!verifyUser) {
+            verifyUser = await ctx.prisma.user.create({
+              data: {
+                email: user.email as string,
+                marathonUserId: request.data.user_id
+              }
+            });
+          }
+          return await ctx.prisma.session.create({
             data: {
-              email: user.email as string,
-              marathonUserId: request.data.user_id
+              userId: verifyUser.id,
+              token: request.data.user_token
             }
           });
         }
-        return await ctx.prisma.session.create({
-          data: {
-            userId: verifyUser.id,
-            token: request.data.user_token
-          }
-        });
-      }
 
-      return null;
+        return null;
+      } catch (err: any) {
+        if (err.response.status === 403) {
+          throw makeError('Email or password is incorrect', 'wrongCredentials');
+        }
+        return null;
+      }
     }
   })
 ];
