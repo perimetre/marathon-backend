45b8c443971327532edbaed87962011b227078c5
AssisrMatheus <assisr.matheus@gmail.com>
2021-11-18 14:34:18 -0300

    feat: add bundle updater #skip-ci

---
+++ prisma/seedValues/updateBundle.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from '@prisma/client';
+import bundles from './bundles.json';
+
+const db = new PrismaClient();
+
+const main = async () => {
+  let ind = 1;
+  for (const mod of bundles.list) {
+    try {
+      await db.module.update({
+        data: {
+          bundleUrl: mod.bundlePath
+        },
+        where: {
+          partNumber: mod.partNumber
+        }
+      });
+      console.log(`Updating ${ind} of ${bundles.list.length}: ${mod.partNumber}`);
+    } catch (err) {
+      console.log('-------------');
+      console.error(`ERROR ${ind} of ${bundles.list.length}: ${mod.partNumber}`);
+      console.error(err);
+      console.log('-------------');
+    } finally {
+      ind++;
+    }
+  }
+};
+
+let error = false;
+
+main()
+  .catch((e) => {
+    console.error(e);
+    error = true;
+  })
+  .finally(async () => {
+    await db.$disconnect();
+    if (error) process.exit(1);
+  });
