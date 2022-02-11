9c3aee0e077e55a37d6398cf0b1c7e02530ab2bf
AssisrMatheus <assisr.matheus@gmail.com>
2022-02-11 17:01:59 -0300

    fix: turns off sync service for now

--- src/cron.ts
+++ src/cron.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 // import * as cron from 'node-cron';
-import { marathonService } from './services/marathon';
+// import { marathonService } from './services/marathon';
 // import logging from './utils/logging';
 
 const scheduleJobs = async (prisma: PrismaClient): Promise<void> => {
   // const daily = '0 4 * * *';
   //
-  const marathon = marathonService({ db: prisma });
-  await marathon.syncData();
+  // const marathon = marathonService({ db: prisma });
+  // await marathon.syncData();
 
   // cron.schedule(daily, async () => {
   //   console.log(`Daily job (${daily}) `);
